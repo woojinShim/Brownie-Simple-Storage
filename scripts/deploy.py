2bfca8ea8ebb2d922cb5a9abd01d901972da5e5f
Woojin <cmshim22@gmail.com>
2022-01-09 15:34:00 +0900

    ETH-Brownie

---
+++ scripts/deploy.py
@@ -0,0 +1,16 @@
+from brownie import accounts, config, SimpleStorage
+
+
+def deploy_simple_storage():
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
+
+
+def main():
+    deploy_simple_storage()
