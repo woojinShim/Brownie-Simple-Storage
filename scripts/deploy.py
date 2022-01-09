6903a9dac37a1cb26a0c9f0675161654c6b047ec
Woojin <cmshim22@gmail.com>
2022-01-10 00:09:36 +0900

    Read Value

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,8 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrieve()
     print(stored_value)
@@ -10,6 +10,13 @@ def deploy_simple_storage():
     transaction.wait(1)
     updated_stored_value = simple_storage.retrieve()
     print(updated_stored_value)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
